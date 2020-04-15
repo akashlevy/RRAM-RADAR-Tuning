10eb24acfd7f771adece5465e6be696bb71ce9bd
Akash Levy <akashlevy@gmail.com>
2020-04-15 16:28:06 -0700

    Stuff working

---
+++ data/fppv.py
@@ -0,0 +1,47 @@
+import matplotlib as mpl, numpy as np, pandas as pd, pygmo as pg
+import matplotlib.pyplot as plt
+
+data = pd.read_csv('fppv4-4-14-20.csv', delimiter='\t', names=['addr', 'nreads', 'nsets', 'nresets', 'rf', 'if', 'rlo', 'rhi', 'success'], index_col=False)
+data['npulses'] = data['nsets'] + data['nresets']
+rlos = data['rlo'].unique()
+data['bin'] = data['rlo'].apply(lambda x: np.where(rlos == x)[0][0])
+print data
+
+# LaTEX quality figures 
+mpl.rcParams.update(
+    {
+    'text.usetex': True,
+    'pgf.texsystem': 'lualatex',
+    'pgf.rcfonts': True,
+    }
+)
+plt.rc('font', family='serif', serif='Times')
+
+
+# FPPV Statistics
+print 'Mean pulses:', data['npulses'].mean()
+print 'Stdev pulses:', data['npulses'].std()
+print 'Mean success rate:', data['success'].mean()
+
+# FPPV Mean Pulses
+grouped = data.groupby(['bin'])
+npulses = grouped['npulses']
+npulses_mean = npulses.mean()
+npulses_std = npulses.std()
+npulses_mean.plot.bar(title='FPPV: Mean Pulses per Level', figsize=(4,3), yerr=npulses_std)
+plt.xlabel('Level Number')
+plt.ylabel('Mean Pulses Required')
+plt.tight_layout()
+plt.savefig('fppv-mean-pulses-beststep-bin.eps')
+plt.show()
+
+# FPPV Mean Success Rate
+grouped = data.groupby(['bin'])
+success = grouped['success']
+success_mean = success.mean()
+success_mean.plot.bar(title='FPPV: Success Rate per Level', figsize=(4,3))
+plt.xlabel('Level Number')
+plt.ylabel('Success Rate')
+plt.tight_layout()
+plt.savefig('fppv-mean-success-beststep-bin.eps')
+plt.show()
