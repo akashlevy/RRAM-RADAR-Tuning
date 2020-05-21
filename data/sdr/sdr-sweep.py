cb21d7db739cff07698b042f85bf57cfa2e14b6f
Akash Levy <akashlevy@gmail.com>
2020-05-21 16:54:00 -0700

    Commit

---
+++ data/sdr/sdr-sweep.py
@@ -0,0 +1,83 @@
+import matplotlib as mpl, numpy as np, pandas as pd, pygmo as pg
+import matplotlib.pyplot as plt
+
+maxpulses = 40
+
+datas = []
+for step in np.arange(0.05, 0.55, 0.05):
+    #data = pd.read_csv('sdr-wl0.06-bl%.2f-sl0.05-5-15-20.csv' % step, delimiter='\t', names=
+    data = pd.read_csv('sdr-wl0.06-bl0.40-sl%.2f-5-15-20.csv' % step, delimiter='\t', names=
+    ['addr', 'nreads', 'nsets', 'nresets', 'rf', 'if', 'rlo', 'rhi', 'success'], index_col=False)
+    data['npulses'] = data['nsets'] + data['nresets']
+    data['stepsize'] = step
+    rlos = data['rlo'].unique()
+    data['bin'] = data['rlo'].apply(lambda x: np.where(rlos == x)[0][0])
+    datas.append(data)
+data = pd.concat(datas)
+#data['success'] = data['success'].astype(bool) & (data['npulses'] <= maxpulses)
+#data['npulses'] = data['npulses'].clip(upper=maxpulses)
+print data
+
+
+# LaTEX quality figures 
+mpl.rcParams.update(
+    {
+    'text.usetex': True,
+    'pgf.texsystem': 'lualatex',
+    'pgf.rcfonts': True,
+    }
+)
+plt.rc('font', family='serif', serif='Times')
+
+
+# SDR Mean Step
+grouped = data.groupby(['stepsize'])
+npulses = grouped['npulses']
+npulses_mean = npulses.mean()
+print npulses_mean
+npulses_std = npulses.std()
+#print npulses_std
+npulses_mean.plot.bar(title='SDR: Mean Pulses vs. Step Size', figsize=(4,3), yerr=npulses_std)
+plt.xlabel('Step Size')
+plt.ylabel('Mean Pulses Required')
+plt.tight_layout()
+plt.savefig('sdr-mean-pulses-step.eps')
+plt.show()
+
+
+# SDR Mean Success Rate
+grouped = data.groupby(['stepsize'])
+success = grouped['success']
+success_mean = success.mean()
+print success_mean
+success_mean.plot.bar(title='SDR: Success Rate vs. Step Size', figsize=(4,3))
+plt.xlabel('Step Size')
+plt.ylabel('Success Rate')
+plt.tight_layout()
+plt.savefig('sdr-mean-success-step.eps')
+plt.show()
+
+
+# SDR Mean Step
+grouped = data[(data['stepsize'] >= 0.44) & (data['stepsize'] <= 0.46)].groupby(['bin'])
+npulses = grouped['npulses']
+npulses_mean = npulses.mean()
+npulses_std = npulses.std()
+npulses_mean.plot.bar(title='SDR: Mean Pulses per Level', figsize=(4,3), yerr=npulses_std)
+plt.xlabel('Level Number')
+plt.ylabel('Mean Pulses Required')
+plt.tight_layout()
+plt.savefig('sdr-mean-pulses-beststep-bin.eps')
+plt.show()
+
+# SDR Mean Success Rate
+grouped = data[(data['stepsize'] >= 0.44) & (data['stepsize'] <= 0.46)].groupby(['bin'])
+success = grouped['success']
+success_mean = success.mean()
+print success_mean
+success_mean.plot.bar(title='SDR: Success Rate per Level', figsize=(4,3))
+plt.xlabel('Level Number')
+plt.ylabel('Success Rate')
+plt.tight_layout()
+plt.savefig('sdr-mean-success-beststep-bin.eps')
+plt.show()
