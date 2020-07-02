2f6658b79a93d8004a42414353b19c7e282c149a
Akash Levy <akashlevy@gmail.com>
2020-07-01 18:14:24 -0700

    Revert "1T4R results for now"
    This reverts commit 1863ad116e1c96e20b6be837cf49b1847904f085.

--- data/retention/dist.py
+++ data/retention/dist.py
@@ -3,57 +3,60 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 # Define CB size
-cbsize = 32
-
-# # Load data
-# names = ['addr', 'nreads', 'nsets', 'nresets', 'rf', 'if', 'rlo', 'rhi', 'success', 'attempts1', 'attempts2']
-# data = pd.read_csv('data/writeispp.csv', delimiter='\t', names=names, index_col=False)
-# rlos = data['rlo'].unique()
-# print sorted(rlos)
-# data['bin'] = data['rlo'].apply(lambda x: np.where(rlos == x)[0][0])
-# data['rf'] = data['rf']/1000
-# data['g'] = 1/data['rf']
-
-# ranges = range(32)
-
-# # Conductance plot
-# plt.xlim(0, 0.35)
-# for i in ranges:
-#     rdata = data[data['bin'] == i]
-#     print i
-#     sns.distplot(rdata['g'],kde=False)
-# plt.show()
-
-# # Resistance plot
-# plt.xlim(0, 60)
-# for i in ranges:
-#     rdata = data[data['bin'] == i]
-#     print i
-#     sns.distplot(rdata['rf'],kde=False)
-# plt.show()
-
+cbsize = 16
 
 # Load data
-names = ['rf']
-data = pd.read_csv('data/readisppbake.csv', delimiter='\t', names=names, index_col=False)
+names = ['addr', 'nreads', 'nsets', 'nresets', 'rf', 'if', 'rlo', 'rhi', 'success', 'attempts1', 'attempts2']
+data = pd.read_csv('data/writeispp.csv', delimiter='\t', names=names, index_col=False)
+data = data[data['rlo'] != 5260.0]
+data = data[data['rlo'] != 5560.0]
+data = data[data['rlo'] != 5880.0]
+rlos = data['rlo'].unique()
+print sorted(rlos)
+data['bin'] = data['rlo'].apply(lambda x: np.where(rlos == x)[0][0])
+#data['bin'] = 7 - ( data.index + data.index / cbsize ) % 8 + (data.index % 768) / 256 * 8
 data['rf'] = data['rf']/1000
 data['g'] = 1/data['rf']
-data['bin'] = ( data.index + data.index / cbsize ) % 32
-print data
-
-ranges = [0, 3, 6, 9, 12, 16, 21, 31]
+#data = data.tail(1024)
 
 # Conductance plot
-for i in ranges:
-    plt.xlim(0, 0.35)
+plt.xlim(0, 0.25)
+for i in range(20):
     rdata = data[data['bin'] == i]
     print i
-    sns.distplot(rdata['g'],kde=False, bins=int(np.ceil((rdata['g'].max() - rdata['g'].min())/0.005)))
+    sns.distplot(rdata['g'],kde=False)
 plt.show()
 
 # Resistance plot
 plt.xlim(0, 60)
-for i in ranges:
+for i in range(20):
+    rdata = data[data['bin'] == i]
+    print i
+    sns.distplot(rdata['rf'],kde=False)
+plt.show()
+
+
+# Load data
+names = ['rf']
+data = pd.read_csv('data/readispp.csv', delimiter='\t', names=names, index_col=False)
+data['rf'] = data['rf']/1000
+data['g'] = 1/data['rf']
+data = data.head(768*200)
+data = data.tail(768)
+data['bin'] = 7 - ( data.index + data.index / cbsize ) % 8 + (data.index % 768) / 256 * 8
+print data
+
+# Conductance plot
+for i in [19, 17, 15, 13, 11, 8, 5, 0]:
+    plt.xlim(0, 0.25)
+    rdata = data[data['bin'] == i]
+    print i
+    sns.distplot(rdata['g'],kde=False)
+plt.show()
+
+# Resistance plot
+plt.xlim(0, 60)
+for i in [19, 17, 15, 13, 11, 8, 5, 0]:
     rdata = data[data['bin'] == i]
     print i
     sns.distplot(rdata['rf'],kde=False)
