94b22c76c976a2c36ca5c1d596dbf1947fc2b0a7
Akash Levy <akashlevy@gmail.com>
2020-07-17 04:24:45 -0700

    Remove old ISPP results and update

--- data/sweeps/ispp-wl-sweep.py
+++ data/sweeps/ispp-wl-sweep.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 # Load data
 names = ['addr', 'pw', 'blv', 'wlv', 'ri', 'rf']
-stepsize = 0.02
+stepsize = 0.01
 data = pd.read_csv('data/set-sweep-wl-noreset-200pw-7-16-20.csv', delimiter='\t', names=names)
 print data
 
@@ -40,7 +40,7 @@ stds = rf.std()/1000.
 # Derivative and smoothing
 pts = means.values
 print pts
-x1, x2 = (2.18, 2.22)
+x1, x2 = (2.23, 2.27)
 xsi = (int(round((x1-1)/stepsize)), int(round((x2-1)/stepsize)))
 print xsi
 y1, y2 = pts[xsi[0]], pts[xsi[1]]
@@ -50,12 +50,12 @@ print gradpw
 
 # Plot
 ax = means.plot(title='Fine SET WL Voltage Sweep', logy=False, xlim=(2, 3), ylim=(0, 20), linewidth=2, figsize=(4,3)) #, yerr=stds.unstack(), elinewidth=0.5)
-plt.plot([3*x1-2*x2, 2*x2-x1], [y1-2*gradpw*(x2-x1), y2+gradpw*(x2-x1)], 'r:')
-plt.annotate('Slope: %.1f k$\\Omega$/V' % gradpw, xy=(x1, y1), xytext=(2.4, 30), arrowprops=dict(facecolor='black', shrink=0.1, width=1, headwidth=3, headlength=5), fontsize=11, horizontalalignment='center', verticalalignment='center')
+plt.plot([3*x1-2*x2, 3*x2-2*x1], [y1-2*gradpw*(x2-x1), y2+2*gradpw*(x2-x1)], 'r:')
+plt.annotate('Slope: %.1f k$\\Omega$/V' % gradpw, xy=(x1, y1), xytext=(2.6, 10), arrowprops=dict(facecolor='black', shrink=0.1, width=1, headwidth=3, headlength=5), fontsize=11, horizontalalignment='center', verticalalignment='center')
 plt.xlabel('WL Voltage (V)')
 plt.ylabel('Mean Resistance (k$\\Omega$)')
 leg = plt.legend([''], columnspacing=1, handletextpad=0.5, borderpad=0.2, prop={'size': 11})
-leg.set_title(title='VBL=2V, PW=100ns', prop={'size': 11})
+leg.set_title(title='VBL=2V, PW=200ns', prop={'size': 11})
 plt.tight_layout()
 plt.savefig('figs/ispp-wl-sweep.eps')
 plt.show()
