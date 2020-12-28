193cf075e900428c482a8348a52f05bc74e6a1d0
Akash Levy <akashlevy@gmail.com>
2020-12-28 00:51:27 -0500

    Process

--- simulation/process.py
+++ simulation/process.py
@@ -20,13 +20,14 @@ RESET_VOLTAGE = -2
 # SET (True) or RESET (False)
 SET = True
 TRIP_POINT = SET_VOLTAGE if SET else RESET_VOLTAGE
+knob = 'Vbl' if SET else 'Vsl'
 
 # Regex for extracting Vbl/Vsl and resistor value
-colre = re.compile('/Vdr \(V.l.vdc=([^,]*),R0.r=([^)]*)\) Y')
+colre = re.compile('/Vdr \(V.l.vdc=([^,]*),R0.r=([^)]*)\) Y') if SET else re.compile('/Vdr \(R0.r=([^)]*),V.l.vdc=([^,]*)\) Y')
 
 # Load data and clean up columns (remove repeated X cols and rename to Vwl)
 data = pd.read_csv('sim-results-set.csv') if SET else pd.read_csv('sim-results-reset.csv')
-data.rename(columns={'/Vdr (Vbl.vdc=0,R0.r=1000) X': 'Vwl', '/Vdr (Vsl.vdc=0,R0.r=1000) X': 'Vwl'}, inplace=True)
+data.rename(columns={data.columns[0]: 'Vwl'}, inplace=True)
 droplist = [i for i in data.columns if i.endswith('X')]
 data.drop(droplist, axis=1, inplace=True)
 
@@ -37,7 +38,7 @@ for column in data.columns[1:]:
     if SET:
         (Vbl, R), Vsl = tuple(float(x) for x in colre.match(column).groups()), 0
     else:
-        (Vsl, R), Vbl = tuple(float(x) for x in colre.match(column).groups()), 0
+        (R, Vsl), Vbl = tuple(float(x) for x in colre.match(column).groups()), 0
     data[column] = Vbl - data[column]
     for Vwl, Vr in zip(data['Vwl'], data[column]):
         data2.append([Vwl, Vbl, Vsl, R, Vr])
@@ -47,11 +48,12 @@ data = pd.DataFrame(data2, columns=columns)
 data['R'] = data['R']/1000
 
 # Compute gradients
-knob = 'Vbl' if SET else 'Vsl'
-gdata = data.groupby([knob, 'R'])
-data['dVr/dVwl'] = gdata['Vr'].transform(pd.Series.diff)/gdata['Vwl'].transform(pd.Series.diff)
-gdata = data.groupby(['Vwl', 'R'])
-data['dVr/d{knob}'.format(knob=knob)] = gdata['Vr'].transform(pd.Series.diff)/gdata[knob].transform(pd.Series.diff)
+data = data.sort_values([knob, 'R', 'Vwl'], kind='mergesort')
+gdata = data.groupby(['R', knob])
+data['dVr/dVwl'] = gdata['Vr'].transform(pd.Series.diff)/0.01
+
+gdata = data.groupby(['R', 'Vwl'])
+data['dVr/d{knob}'.format(knob=knob)] = gdata['Vr'].transform(pd.Series.diff)/0.01
 data.fillna(0, inplace=True)
 
 # Setup figure
@@ -59,19 +61,21 @@ plt.figure(figsize=(4,2.5))
 
 # Find nearest point to the trip point
 pd.set_option('display.max_rows', None)
-neardata = data[np.abs(data['Vbl'] - 2) < 1e-5] if SET else data[np.abs(data['Vsl'] - 2.8) < 1e-5]
-neardata['tripdist'] = np.abs(neardata['Vr']-TRIP_POINT)
-indices = neardata.groupby('R')['tripdist'].transform('idxmin')
-neardata = neardata.loc[indices].drop_duplicates()
-neardata.plot('R', 'dVr/dVwl', ax=plt.gca(), marker='.')
-print(neardata)
+#data = data[data['Vwl'] <= 3]
+data['tripdist'] = np.abs(data['Vr']-TRIP_POINT)
+indices = data.groupby(['Vwl','R'])['tripdist'].transform('idxmin')
+neardata = data.loc[indices].drop_duplicates()
+selection = (np.abs(neardata['Vwl'] - 2) < 1e-5) & (np.abs(neardata['R'] - 4) < 1e-5)
+selection |= (np.abs(neardata['Vwl'] - 1.86) < 1e-5) & (np.abs(neardata['R'] - 4.555) < 1e-5)
+selection |= (np.abs(neardata['Vwl'] - 1.68) < 1e-5) & (np.abs(neardata['R'] - 5.063) < 1e-5)
+selection |= (np.abs(neardata['Vwl'] - 1.52) < 1e-5) & (np.abs(neardata['R'] - 5.7) < 1e-5)
+selection |= (np.abs(neardata['Vwl'] - 1.39) < 1e-5) & (np.abs(neardata['R'] - 6.547) < 1e-5)
+selection |= (np.abs(neardata['Vwl'] - 1.27) < 1e-5) & (np.abs(neardata['R'] - 7.788) < 1e-5)
+selection |= (np.abs(neardata['Vwl'] - 1.17) < 1e-5) & (np.abs(neardata['R'] - 11.67) < 1e-5)
+neardata = neardata[selection].sort_values('R')
 
-neardata = data[np.abs(data['Vwl'] - 3) < 1e-5] if SET else data[np.abs(data['Vwl'] - 3.5) < 1e-5]
-neardata['tripdist'] = np.abs(neardata['Vr']-TRIP_POINT)
-indices = neardata.groupby('R')['tripdist'].transform('idxmin')
-neardata = neardata.loc[indices].drop_duplicates()
-neardata.plot('R', 'dVr/d{knob}'.format(knob=knob), ax=plt.gca(), marker='.')
 print(neardata)
+neardata.plot('R', ['dVr/dVwl', 'dVr/d{knob}'.format(knob=knob)], ax=plt.gca(), marker='.')
 
 plt.xlabel('$R$ (k$\\Omega$)')
 plt.ylabel('Voltage Gain')
