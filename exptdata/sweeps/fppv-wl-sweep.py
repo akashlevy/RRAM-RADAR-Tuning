566ab0829714cb286610f8c7f2a238a90090b724
Akash Levy <akashlevy@gmail.com>
2021-04-12 02:19:29 -0700

    FPPV sweep updates

--- exptdata/sweeps/fppv-wl-sweep.py
+++ exptdata/sweeps/fppv-wl-sweep.py
@@ -1,14 +1,14 @@
-import matplotlib as mpl, numpy as np, pandas as pd, pygmo as pg
+import matplotlib as mpl, numpy as np, pandas as pd
 import matplotlib.pyplot as plt
 
-chipnum = 2
+chipnum = 1
 if chipnum == 1:
     data = pd.read_csv('data/set-sweep-wl-200pw-7-16-20.csv', delimiter='\t', names=['addr', 'pw', 'blv', 'wlv', 'ri', 'rf']) # chip1
 if chipnum == 2:
     data = pd.read_csv('data/set-sweep-wl-200ns-step-0.01-8-9-20.csv', delimiter='\t', names=['addr', 'pw', 'blv', 'wlv', 'ri', 'rf']) # chip2
 data = data[data['pw'] == 200]
 data = data[data['blv'] == 2]
-print data
+print(data)
 
 # LaTEX quality figures 
 mpl.rcParams.update(
@@ -34,7 +34,7 @@ grouped = data.groupby(['wlv'])
 rf = grouped['rf']
 medians = rf.median()/1000.
 stds = rf.std()/1000.
-print medians
+print(medians)
 medians.to_csv('results/fppv%s.csv' % chipnum)
 
 pts = medians.values
@@ -45,17 +45,18 @@ if chipnum == 2:
 vis = [int(round((v-1.5)/0.01)) for v in vs]
 rs = [pts[vi] for vi in vis]
 data = zip(range(7), vs, rs)
-print data
+print(data)
 
 # Plot WL voltage and selections
-medians.plot(title='FPPV WL Voltage Selection', logy=False, xlim=(2, 3.2), ylim=(0, 15), linewidth=2, figsize=(4,3))
+medians.plot(title='FPPV Tuning', logy=False, xlim=(2, 3.2), ylim=(0, 15), linewidth=2, figsize=(3.2,3))
 for i, v, r in data:
     ha = 'left' if i not in [0, 1] else 'right'
     plt.annotate('Range %i: %.2fV' % (i,v), xy=(v, r), xytext=(v, (i+2)*1.7 if i not in [0,1] else (3.7 if i == 1 else 2)), arrowprops=dict(facecolor='black', shrink=0.05, width=1, headwidth=1, headlength=1, linestyle='dotted'), fontsize=10, horizontalalignment=ha, verticalalignment='center')
 plt.xlabel('WL Voltage (V)')
 plt.ylabel('Median Resistance (k$\\Omega$)')
-leg = plt.legend([''], handletextpad=0.5, borderpad=0.2)
-leg.set_title(title='VBL=2V\nPW=200ns', prop={'size': 11})
+#leg = plt.legend([''], handletextpad=0.5, borderpad=0.2)
+#leg.set_title(title='VBL=2V\nPW=200ns', prop={'size': 11})
 plt.tight_layout()
 plt.savefig('figs/fppv-wl-sweep.eps')
+plt.savefig('figs/fppv-wl-sweep.pdf')
 plt.show()
